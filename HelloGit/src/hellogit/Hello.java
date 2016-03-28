ab2808c44416cd4bbc163f64e3b72c36030e002a
lisijia <lisj@cfischina.com>
2016-03-28 11:30:43 +0800

    3

--- HelloGit/src/hellogit/Hello.java
+++ HelloGit/src/hellogit/Hello.java
@@ -3,7 +3,7 @@ package hellogit;
 public class Hello {  
 	public static void main(String[] args) {
 		System.out.println("sss");
-		System.out.println("bug001");
+		System.out.println("bug001"); 
 		//sssfff       
 	}
 }
